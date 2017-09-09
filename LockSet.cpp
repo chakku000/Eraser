ddf98bd95f735271e52bcaaa77e02dc68f6f1837
chakku <chakkurai0724@outlook.jp>
2017-09-09 20:45:08 +0900

    ShadowWordの実装

--- LockSet.cpp
+++ LockSet.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <bitset>
 #include <map>
+#include <cstdint>
 
 
 constexpr uint32_t max_lock = 128;
-using Locks = bitset<max_lock>;      // ロックの最大個数を128とする
+using Locks = std::bitset<max_lock>;      // ロックの最大個数を128とする
 
 
 template<typename Key,typename Val>
@@ -57,6 +58,69 @@ struct LocksHeld{
         }
 };
 
+struct ShadowWord{
+    private:
+        enum State{
+            Virgin,
+            Exclusive,
+            Shared,
+            SharedModified,
+        };
+        Locks lockset;
+        uint32_t th;
+        State state;
+    public:
+        ShadowWord(){
+            state = Virgin;
+            lockset.set();
+        }
+        ShadowWord(uint32_t thread_id){
+            th = thread_id;
+            state = Virgin;
+            lockset.set();
+        }
+
+        void read_access(uint32_t thread_id , Locks locksheld){
+            if(state == Virgin){
+                state = Exclusive;
+            }else if(state == Exclusive){
+                if(th != thread_id){
+                    state = Shared;
+                }
+            }else if(state == Shared){
+                lockset &= locksheld;
+            }else if(state == SharedModified){
+                lockset &= locksheld;
+                if(!lockset.any()){
+                    // 警告
+                    std::cerr << "Candidate Lock Set is empty" << std::endl;
+                }
+            }
+        }
+
+        void write_access(uint32_t thread_id,Locks locksheld){
+            if(state == Virgin){
+                state = Exclusive;
+            }else if(state == Exclusive){
+                if(th != thread_id){
+                    state = SharedModified;
+                }
+            }else if(state == Shared){
+                state = SharedModified;
+                lockset &= locksheld;
+            }else if(state == SharedModified){
+                lockset &= locksheld;
+                if(!lockset.any()){
+                    // 警告
+                    std::cerr << "Candidate Lock Set is empty" << std::endl;
+                }
+            }
+        }
+};
 
 LockManager<pthread_mutex_t*,uint32_t> lockmanager;
 LocksHeld locks_held;
+
+int main(){
+
+}
