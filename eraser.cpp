4ec5434f865aee39e109f7a90a198858667b3d28
chakku <chakkurai0724@outlook.jp>
2017-09-13 00:49:58 +0900

    Lockset.cppから必要なものをeraser.cppに移植

--- eraser.cpp
+++ eraser.cpp
@@ -78,11 +78,103 @@ struct LocksHeld{/*{{{*/
         }
 };/*}}}*/
 
+
+/* ------------------------------------------------------------*
+ * shadow word (詳細は論文)
+ * ------------------------------------------------------------*/
+
+struct ShadowWord{/*{{{*/
+    private:
+        enum State{
+            Virgin,
+            Exclusive,
+            Shared,
+            SharedModified,
+        };
+        Locks lockset;
+        uint32_t th;
+        State state;
+    public:
+        /*------------------------------------------------------------------------------*/
+        /* Constructor                                                                  */
+        /* 初期状態をExclusiveにすることで事前にアドレスを保持する必要をなくす          */
+        /*------------------------------------------------------------------------------*/
+        ShadowWord(){
+            state = Exclusive;
+            lockset.set();
+        }
+        ShadowWord(uint32_t thread_id){
+            th = thread_id;
+            state = Exclusive;
+            lockset.set();
+        }
+
+        /* -----------------------------------*/
+        /* Update state and candidate lockset */
+        /* -----------------------------------*/
+        void read_access(uint32_t thread_id , Locks locksheld){
+            if(state == Virgin){
+                state = Exclusive;
+                th = thread_id;
+            }else if(state == Exclusive){
+                if(th != thread_id){
+                    state = Shared;
+                    lockset &= locksheld;
+                }
+            }else if(state == Shared){
+                lockset &= locksheld;
+            }else if(state == SharedModified){
+                lockset &= locksheld;
+                if(!lockset.any()){
+                    // 警告
+                    std::cerr << "Candidate Lock Set is empty" << std::endl;
+                }
+            }
+        }
+
+        void write_access(uint32_t thread_id,Locks locksheld){
+            if(state == Virgin){
+                state = Exclusive;
+                th = thread_id;
+            }else if(state == Exclusive){
+                if(th != thread_id){
+                    state = SharedModified;
+                    lockset &= locksheld;
+                }
+            }else if(state == Shared){
+                state = SharedModified;
+                lockset &= locksheld;
+            }else if(state == SharedModified){
+                lockset &= locksheld;
+                if(!lockset.any()){
+                    // 警告
+                    std::cerr << "Candidate Lock Set is empty" << std::endl;
+                }
+            }
+        }
+
+        void print(){
+            std::string s;
+            if(state == Virgin) s = "Virgin";
+            else if(state == Exclusive) s = "Exclusive";
+            else if(state == Shared) s = "Shared";
+            else if(state == SharedModified) s = "SharedModified";
+
+            std::cout << "state = " << s << std::endl;
+            std::cout << "C(v) = " << lockset << std::endl;
+            if(state == Exclusive) std::cout << "thread = " << th << std::endl;
+
+            std::cout << std::endl;
+        }
+};/*}}}*/
+
+
 /* ===================================================================== */
 /*      Grobal Variable                                                  */
 /* ===================================================================== */
 LockManager<pthread_mutex_t*,uint32_t> lockmanager;
 LocksHeld locks_held;
+std::map<uint32_t,ShadowWord> candidateLockset; // key : 変数のアドレス , val : 変数のshadow word
 PIN_LOCK pinlock;
 
 
