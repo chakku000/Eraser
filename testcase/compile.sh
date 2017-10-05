a0eb74e9ba55ea729f6c8c540976dbac7c5dbab5
chakku <chakkurai0724@outlook.jp>
2017-10-05 15:16:52 +0900

    fix testcase compiler

---
+++ testcase/compile.sh
@@ -0,0 +1,16 @@
+#/bin/sh
+# コンパイル対象の拡張子
+ext="*.c"
+
+#特定拡張子のファイルをコンパイルする
+compile(){
+    for fName in `find . -name "$ext"`
+    do
+        name=`echo $fName | sed -e "s/\.c//" | sed -e "s/\.\///"`
+        gcc -pthread -o $name $fName
+    done
+}
+
+echo "#########  Compile Start ##########"
+compile
+echo "#########   Compile END  ##########"
